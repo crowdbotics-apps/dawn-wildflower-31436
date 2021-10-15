af77b399965eafc5face4b64b106ecc45a34a592
Crowdbotics <team@crowdbotics.com>
2021-10-15 05:01:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dawn_wildflower_31436",
+  url: "https://dawn_wildflower_31436.botics.co",
+  api: "https://dawn_wildflower_31436.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
